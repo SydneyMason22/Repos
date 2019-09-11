5977fa6401bca2657dcd158d27051a4dd0e8f321
Sydney Mason <syd_mason22@yahoo.com>
2019-09-10 21:44:51 -0400

    Updated Restaurant site

---
+++ html/luckySevens.js
@@ -0,0 +1,40 @@
+function playGame() {
+	var begBet = Number(document.forms["luckySevens"]["begBet"].value);
+	if (begBet <= 0) {
+		alert("Starting bet must be greater than zero.");
+		document.forms["luckySevens"]["begBet"].value = "";
+		document.forms["luckySevens"]["begBet"].focus();
+		return false;
+	}
+	var betArray = [begBet];
+	var rollCounter = 0;
+	for (var gameMoney = begBet; gameMoney >= 1; rollCounter++) {
+		dieOne = Math.ceil(Math.random() * (1 + 6 - 1));		
+		dieTwo = Math.ceil(Math.random() * (1 + 6 - 1));
+		if (Number(dieOne + dieTwo) == 7) {
+			gameMoney += 4;
+			betArray[betArray.length] = gameMoney;
+		} else {
+			gameMoney -= 1;
+			betArray[betArray.length] = gameMoney;
+		}
+	}
+	
+	var maxWin = 0;
+	for (var indexCounter = 0; indexCounter < betArray.length; indexCounter++) {
+		if (betArray[indexCounter] > maxWin) {
+			maxWin = betArray[indexCounter];
+		}
+	}
+	document.getElementById("results").style.display = "block";
+	//Used .toFixed(2) function to create a string of 2 decimals even though the game only recognizes whole numbers.
+	document.getElementById("startBet").innerText = ("$" + begBet.toFixed(2));
+	document.getElementById("totalRolls").innerText = rollCounter;
+	document.getElementById("highAmount").innerText = ("$" + maxWin.toFixed(2));
+	document.getElementById("highRollCount").innerText = betArray.indexOf(maxWin);
+	document.getElementById("submitButton").innerText = "Play Again";
+	document.forms["luckySevens"]["begBet"].focus();
+	/* Used console.log(winArray); here during testing to see what the overall score was after each roll.
+	Otherwise, could have skipped using an array and tracked the max money held at each roll within first for loop's if. */
+	return false;
+}
